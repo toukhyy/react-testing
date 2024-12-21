1dfed1b27fd0e0ab348cf286c4b728a825076e9c
toukhyy <tokhy104@gmail.com>
2024-12-21 15:10:05 +0200

    setting up testing env, adding greet test.

---
+++ tests/components/Great.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
+
+describe("Great", () => {
+  it("should render hello `name`, if name os provided", () => {
+    render(<Greet name="John" />);
+
+    const heading = screen.getByRole("heading");
+
+    expect(heading).toHaveTextContent("Hello John");
+
+    expect(heading).toHaveTextContent(/John/i);
+  });
+
+  it("should render login button, if name not provided", () => {
+    render(<Greet />);
+
+    const btn = screen.getByRole("button");
+
+    expect(btn).toBeInTheDocument();
+
+    expect(btn).toHaveTextContent("Login");
+  });
+});
